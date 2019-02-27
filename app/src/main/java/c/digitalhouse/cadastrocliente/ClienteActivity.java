275008ef044a36151c66bb7dac4cfdd034a0b724
Rosana <rosanaflag@gmail.com>
2019-02-27 11:06:04 -0300

    nn

---
+++ app/src/main/java/c/digitalhouse/cadastrocliente/ClienteActivity.java
@@ -0,0 +1,14 @@
+package c.digitalhouse.cadastrocliente;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClienteActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.cad_clientes );
+
+
+    }
+}
